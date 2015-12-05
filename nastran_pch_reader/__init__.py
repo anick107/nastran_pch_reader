59875e77df7efbe60be17bcf137554789880ca51
anick107 <nick.asmolovsky@gmail.com>
2015-12-05 17:05:54 +0100

    Reorganize package. Store frequency and subcase data separately in order to reduce memory requirements.

---
+++ nastran_pch_reader/__init__.py
@@ -0,0 +1,197 @@
+"""
+Parser for the NASTRAN punch files.
+
+Supported formats:
+- frequency response analysis
+-- displacement
+-- acceleration
+-- element forces for CBUSH, CELAS
+-- MPCF
+-- SPCF
+Supported output types: real/imaginary, mag/phase
+
+
+- quasi-static results
+-- displacement
+-- element forces for CBUSH, CELAS
+-- MPCF
+-- SPCF
+
+"""
+import cmath
+
+
+def dispatch_parse(output, data_chunks):
+    num = int(len(data_chunks) / 2)
+    if len(data_chunks) % 2 != 0:
+        raise ValueError('Wrong number of chunks!',
+                         'Output: %s, num of chunks: %d' % (output, len(data_chunks)))
+
+    if output == 'MAGNITUDE-PHASE':
+        return [data_chunks[i]*cmath.exp(1j*data_chunks[i+num]) for i in range(num)]
+    elif output == 'REAL-IMAGINARY':
+        return [data_chunks[i] + 1j*data_chunks[i+num] for i in range(num)]
+    else:
+        return [data_chunks[i] for i in range(len(data_chunks))]
+
+
+def parse_data_chunks(request, output, entity_type_id, data_chunks):
+    valid_requests = ['ACCELERATION', 'DISPLACEMENTS', 'MPCF', 'SPCF', 'ELEMENT FORCES', 'ELEMENT STRAINS']
+
+    if request in valid_requests:
+        return dispatch_parse(output, data_chunks)
+    else:
+        raise NotImplementedError("Request %s is not implemented", request)
+
+
+class PchParser:
+    def reset_current_frame(self):
+        self.current_data_chunks = []
+        self.is_frequency_response = False
+        self.output_sort = 0
+        self.current_subcase = 0
+        self.current_output = 0
+        self.current_frequency = 0
+        self.current_entity_id = 0
+        self.current_entity_type_id = 0
+
+    def __init__(self, filename):
+        # define the dictionary
+        self.parsed_data = {'FREQUENCY': {}, 'SUBCASES': set()}
+
+        # initiate current frame
+        self.reset_current_frame()
+
+        # start reading
+        with open(filename, 'r') as pch:
+            # read only first 72 characters from the punch file
+            for line in pch:
+                line = line[0:72]
+
+                # reset all variables
+                if line.startswith('$TITLE   ='):
+                    # insert the last frame remaining in memory
+                    self.insert_current_frame()
+                    # reset the frame
+                    self.reset_current_frame()
+
+                # parse the subcase
+                if line.startswith('$SUBCASE ID ='):
+                    self.current_subcase = int(line[13:].strip())
+                    self.parsed_data['SUBCASES'].add(self.current_subcase)
+
+                # identify NASTRAN request
+                if line.startswith('$DISPLACEMENTS'):
+                    self.request = 'DISPLACEMENTS'
+                elif line.startswith('$ACCELERATION'):
+                    self.request = 'ACCELERATION'
+                elif line.startswith('$MPCF'):
+                    self.request = 'MPCF'
+                elif line.startswith('$SPCF'):
+                    self.request = 'SPCF'
+                elif line.startswith('$ELEMENT FORCES'):
+                    self.request = 'ELEMENT FORCES'
+                elif line.startswith('$ELEMENT STRAINS'):
+                    self.request = 'ELEMENT STRAINS'
+
+                # identify output type
+                if line.startswith('$REAL-IMAGINARY OUTPUT'):
+                    self.current_output = 'REAL-IMAGINARY'
+                elif line.startswith('$MAGNITUDE-PHASE OUTPUT'):
+                    self.current_output = 'MAGNITUDE-PHASE'
+                elif line.startswith('REAL OUTPUT'):
+                    self.current_output = 'REAL'
+
+                # parse of frequency response results
+                if line.find('IDENTIFIED BY FREQUENCY') != -1:
+                    self.is_frequency_response = True
+                    self.output_sort = 2
+                elif line.find('$FREQUENCY =') != -1:
+                    self.is_frequency_response = True
+                    self.output_sort = 1
+
+                # parse entity id
+                if line.startswith('$POINT ID ='):
+                    self.current_entity_id = int(line[11:23].strip())
+                elif line.startswith('$ELEMENT ID ='):
+                    self.current_entity_id = int(line[13:23].strip())
+                elif line.startswith('$FREQUENCY = '):
+                    self.current_frequency = float(line[12:28].strip())
+
+                # parse element type
+                if line.startswith('$ELEMENT TYPE ='):
+                    self.current_entity_type_id = int(line[15:27].strip())
+
+                # ignore other comments
+                if line.startswith('$'):
+                    continue
+
+                if self.request == 0:
+                    raise NotImplementedError('Unknown request in the frame',
+                                              'Check request type for the following string: %s' % line)
+
+                # start data parsing
+                line = line.replace('G', ' ')
+                if line.startswith('-CONT-'):
+                    line = line.replace('-CONT-', '')
+                    self.current_data_chunks += [float(_) for _ in line.split()]
+                else:
+                    # insert the last frame
+                    self.insert_current_frame()
+
+                    # update the last frame with a new data
+                    self.current_data_chunks = [float(_) for _ in line.split()]
+
+            # last block remaining in memory
+            self.insert_current_frame()
+
+    def insert_current_frame(self):
+        # last block remaining in memory
+        if len(self.current_data_chunks) > 0:
+            if self.request not in self.parsed_data:
+                self.parsed_data[self.request] = {}
+
+            if self.current_subcase not in self.parsed_data[self.request]:
+                self.parsed_data[self.request][self.current_subcase] = {}
+                self.parsed_data['FREQUENCY'][self.current_subcase] = {}
+
+            if self.is_frequency_response:
+                values = []
+                # incremented by frequency, entity is given
+                if self.output_sort == 2:
+                    self.current_frequency = self.current_data_chunks[0]
+                    values = self.current_data_chunks[1:]
+
+                # incremented by entity, frequency is given
+                elif self.output_sort == 1:
+                    self.current_entity_id = int(self.current_data_chunks[0])
+                    values = self.current_data_chunks[1:]
+
+                # insert frequency in the database
+                if self.current_frequency not in self.parsed_data['FREQUENCY'][self.current_subcase]:
+                    self.parsed_data['FREQUENCY'][self.current_subcase][self.current_frequency] = len(self.parsed_data['FREQUENCY'][self.current_subcase])
+
+                # ensure that this dictionary exists
+                if self.current_entity_id not in self.parsed_data[self.request][self.current_subcase]:
+                    self.parsed_data[self.request][self.current_subcase][self.current_entity_id] = []
+
+                vector = parse_data_chunks(self.request, self.current_output, self.current_entity_type_id, values)
+                self.parsed_data[self.request][self.current_subcase][self.current_entity_id].append(vector)
+
+            else:
+                self.current_entity_id = int(self.current_data_chunks[0])
+                self.parsed_data[self.request][self.current_subcase][self.current_entity_id] = \
+                    parse_data_chunks(self.request,
+                                      self.current_output,
+                                      self.current_entity_type_id,
+                                      self.current_data_chunks[1:])
+
+    def health_check(self):
+        frequency_steps = []
+        for subcase in self.parsed_data['SUBCASES']:
+            frequency_steps.append(len(self.parsed_data['FREQUENCY'][subcase]))
+        assert min(frequency_steps) == max(frequency_steps)
+
+    def get_data(self):
+        self.health_check()
+        return self.parsed_data
--- nastran_pch_reader/tests/test_parse_pch.py
+++ nastran_pch_reader/tests/test_parse_pch.py
@@ -1,10 +1,10 @@
 import unittest
-import parse_pch
+import nastran_pch_reader
 
 
 class TestStaticOutput(unittest.TestCase):
     def setUp(self):
-        parser = parse_pch.PchParser('tests/nsa_1012_ug.pch')
+        parser = nastran_pch_reader.PchParser('data/nsa_1012_ug.pch')
         self.data = parser.get_data()
 
     def tearDown(self):
@@ -17,30 +17,14 @@ class TestStaticOutput(unittest.TestCase):
     check boundary values
     """
     def testValues(self):
-        self.assertDictEqual(self.data['ELEMENT FORCES'][1][66008947],
-                             {
-                                 'FX': -0.3086368,
-                                 'FY': -0.04711712,
-                                 'FZ': 0.0008220575,
-                                 'MX': -8.683135e-06,
-                                 'MY': 7.214578e-05,
-                                 'MZ': 9.478482e-05
-                             })
+        self.assertListEqual(self.data['ELEMENT FORCES'][1][66008947], [-0.3086368,  -0.04711712, 0.0008220575,-8.683135e-06, 7.214578e-05, 9.478482e-05])
 
-        self.assertDictEqual(self.data['ELEMENT FORCES'][3][70256346],
-                             {
-                                 'FX': -5.520462E-02,
-                                 'FY': -5.823757E-02,
-                                 'FZ': -2.389058E-02,
-                                 'MX': 1.035513E-04,
-                                 'MY': -1.877879E-04,
-                                 'MZ': -3.983908E-05
-                             })
+        self.assertListEqual(self.data['ELEMENT FORCES'][3][70256346], [-5.520462E-02,-5.823757E-02,-2.389058E-02, 1.035513E-04,-1.877879E-04,-3.983908E-05])
 
 
 class TestFrequencyResponseOutputSort1(unittest.TestCase):
     def setUp(self):
-        parser = parse_pch.PchParser('tests/sine_gyro.pch')
+        parser = nastran_pch_reader.PchParser('data/sine_gyro.pch')
         self.data = parser.get_data()
 
     def tearDown(self):
@@ -53,17 +37,18 @@ class TestFrequencyResponseOutputSort1(unittest.TestCase):
     check boundary values
     """
     def testValues(self):
-        self.assertEqual(abs(self.data['ACCELERATION'][1][3000001][0.2000000E+02]['TX']), 4.064976E+00)
-        self.assertEqual(abs(self.data['ACCELERATION'][3][3000006][0.2000000E+04]['TZ']), 1.000748E-02)
+        self.assertEqual(abs(self.data['ACCELERATION'][1][3000001][0][0]), 4.064976E+00)
+        self.assertEqual(abs(self.data['ACCELERATION'][3][3000006][2276][2]), 1.000748E-02)
 
     def testNodes(self):
         self.assertListEqual(sorted(self.data['ACCELERATION'][1].keys()), [3000001, 3000002, 3000003, 3000004, 3000005, 3000006])
         self.assertListEqual(sorted(self.data['ACCELERATION'][2].keys()), [3000001, 3000002, 3000003, 3000004, 3000005, 3000006])
         self.assertListEqual(sorted(self.data['ACCELERATION'][3].keys()), [3000001, 3000002, 3000003, 3000004, 3000005, 3000006])
 
+
 class TestFrequencyResponseOutputSort2Displacement(unittest.TestCase):
     def setUp(self):
-        parser = parse_pch.PchParser('tests/SGEO_PFM_79_v08_sine_closures.pch')
+        parser = nastran_pch_reader.PchParser('data/SGEO_PFM_79_v08_sine_closures.pch')
         self.data = parser.get_data()
 
     def tearDown(self):
@@ -76,10 +61,11 @@ class TestFrequencyResponseOutputSort2Displacement(unittest.TestCase):
     check boundary values
     """
     def testValues(self):
-        self.assertEqual(self.data['DISPLACEMENTS'][1][200001][5.000000E+00]['TX'], -1.057051E-02 + 1j*8.361023E-06)
-        self.assertEqual(self.data['DISPLACEMENTS'][2][300002][7.000000E+00]['TY'], -5.722504E-03 + 1j*1.270270E-05)
-        self.assertEqual(self.data['DISPLACEMENTS'][3][1252024][1.000000E+02]['RZ'], 4.814706E-05 + 1j*-6.976266E-05)
+        self.assertEqual(self.data['DISPLACEMENTS'][1][200001][0][0], -1.057051E-02 + 1j*8.361023E-06)
+        self.assertEqual(self.data['DISPLACEMENTS'][2][300002][4][1], -5.722504E-03 + 1j*1.270270E-05)
+        self.assertEqual(self.data['DISPLACEMENTS'][3][1252024][1482][5], 4.814706E-05 + 1j*-6.976266E-05)
 
 
 if __name__ == '__main__':
     unittest.main()
+
